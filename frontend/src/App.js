53520e80f24460def03373b7e8df8ccf1b78ba06
omkargaikwad1995 <omkargaikwad1221@gmail.com>
2022-12-19 16:22:13 +0530

    vid-01 create react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Comsense Cart</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
