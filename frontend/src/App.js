a1e6520f859c767a03b0250af3666c986a1862c6
omkargaikwad1995 <omkargaikwad1221@gmail.com>
2022-12-23 16:03:15 +0530

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,57 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Homescreen from './screens/Homescreen';
+import ProductScreen from './screens/ProductScreen';
+import Badge from 'react-bootstrap/Badge';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useContext } from 'react';
+import { Store } from './Store';
+import CartScreen from './screens/CartScreen';
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">Comsense Cart</a>
-      </header>
-      <main>List Products</main>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <div className="App">
+          <header className="App-header">
+            <Navbar bg="dark" variant="dark">
+              <Container>
+                <LinkContainer to="/">
+                  <Navbar.Brand>Comsense Cart</Navbar.Brand>
+                </LinkContainer>
+                <Nav className="me-auto">
+                  <Link to="/cart" className="nav-link">
+                    Cart
+                    {cart.cartItems.length > 0 && (
+                      <Badge pill bg="danger">
+                        {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                      </Badge>
+                    )}
+                  </Link>
+                </Nav>
+              </Container>
+            </Navbar>
+          </header>
+          <main>
+            <Container className="mt-4 mb-5 ">
+              <Routes>
+                <Route path="/product/:slug" element={<ProductScreen />} />
+                <Route path="/" element={<Homescreen />} />
+                <Route path="/cart" element={<CartScreen />} />
+              </Routes>
+            </Container>
+          </main>
+          <footer>
+            <div className="text-center">All Rights Reserved</div>
+          </footer>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
