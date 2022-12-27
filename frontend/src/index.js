e83b6e9536661f7d68c2dbe573d06ef254aa5eb0
omkargaikwad1995 <omkargaikwad1221@gmail.com>
2022-12-27 18:49:44 +0530

    smartech script added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StoreProvider } from './Store';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDVH1sybkMu7w7xvWN0oJE7-nkRh7G1AMg',
+  authDomain: 'comsense-cart.firebaseapp.com',
+  projectId: 'comsense-cart',
+  storageBucket: 'comsense-cart.appspot.com',
+  messagingSenderId: '238060138678',
+  appId: '1:238060138678:web:ae7ced4f5139f94fccf617',
+  measurementId: 'G-YE05G4872E',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
