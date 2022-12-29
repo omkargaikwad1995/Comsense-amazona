bd05b378b7d3a454fbdedc39453bc2802f9a8f3c
omkargaikwad1995 <omkargaikwad1221@gmail.com>
2022-12-29 12:06:20 +0530

    siteid changed

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,9 +1,9 @@
 var config = {
-    apiKey: 'AIzaSyDVH1sybkMu7w7xvWN0oJE7-nkRh7G1AMg',
-    messagingSenderId: '238060138678',
-    appId: '1:238060138678:web:ae7ced4f5139f94fccf617',
-    projectId: 'comsense-cart',
-    user_key: 'ADGMOT35CHFLVDHBJNIG50K96BCGEFS4PM141I5KAA9A4PGTMEUG',
-    siteid: '2ba4142301950c648fd3df58cc8d0b42'
+  apiKey: 'AIzaSyDVH1sybkMu7w7xvWN0oJE7-nkRh7G1AMg',
+  messagingSenderId: '238060138678',
+  appId: '1:238060138678:web:ae7ced4f5139f94fccf617',
+  projectId: 'comsense-cart',
+  user_key: 'ADGMOT35CHFLVDHBJNIG50K96BCGEFS4PM141I5KAA9A4PGTMEUG',
+  siteid: '440da7afe6897aee092650976ba485b5',
 };
 importScripts('//cdnt.netcoresmartech.com/swv4.js');
